9c012a5f421fdc0daf6165d53bed25408bb1f3a5
binodnp@outlook.com <binodnp@outlook.com>
2017-01-10 01:50:25 +0545

    Routien development.

---
+++ DAL/PlStatement/IPlStatement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MixERP.Finance.QueryModels;
+
+namespace MixERP.Finance.DAL.PlStatement
+{
+    public interface IPlStatement
+    {
+        Task<IEnumerable<dynamic>> GetAsync(string tenant, PlAccountQueryModel query);
+    }
+}
---
+++ DAL/PlStatement/PostgreSQL.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Frapid.DataAccess;
+using MixERP.Finance.QueryModels;
+using Newtonsoft.Json;
+
+namespace MixERP.Finance.DAL.PlStatement
+{
+    public sealed class PostgreSQL : IPlStatement
+    {
+        public async Task<IEnumerable<dynamic>> GetAsync(string tenant, PlAccountQueryModel query)
+        {
+            string sql = "SELECT * FROM finance.get_profit_and_loss_statement(@0::date,@1::date,@2::integer,@3::integer,@4::integer,@5::boolean)";
+            string json =  await Factory.ScalarAsync<string>(tenant, sql, query.From, query.To, query.UserId, query.OfficeId, query.Factor, query.Compact).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject< IEnumerable<dynamic>>(json);
+        }
+    }
+}
